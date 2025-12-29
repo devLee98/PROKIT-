67cb8fcd78c0bcc5ac5b98ed5d2e8096487f70a3
devLee98 <iiyoungchan@naver.com>
2025-12-29 18:12:41 +0900

    fix : 리셋버튼 초기화시 타이머 쿼리 삭제

--- src/hooks/mutation/use-delete-timer.ts
+++ src/hooks/mutation/use-delete-timer.ts
@@ -1,8 +1,12 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteTimer } from '../../api/timer';
 
 export function useDeleteTimer() {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: deleteTimer,
+    onSuccess: () => {
+      queryClient.removeQueries({ queryKey: ['timer'] });
+    },
   });
 }
